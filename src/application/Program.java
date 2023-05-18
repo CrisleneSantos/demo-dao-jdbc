8f7c0e90d9e39398f752e998bbcfea54d26db0e0
Crislene Santos <28mariavalentina@gmail.com>
2023-05-18 19:36:07 -0300

    finfById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,7 @@
 package application;
 
-import java.util.Date;
-import java.util.Locale;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,12 +9,14 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Locale.setDefault(Locale.US);
+		//Locale.setDefault(Locale.US);
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Jose", "jose@@mail.com", new Date(), 3000.0, obj);
+		//Department obj = new Department(1, "Books");
+		//Seller seller = new Seller(21, "Jose", "jose@@mail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
