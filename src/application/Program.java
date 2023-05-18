60859b31a796eed6f54cc4e5f9c4912b78d2099d
Crislene Santos <28mariavalentina@gmail.com>
2023-05-18 12:33:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Locale.setDefault(Locale.US);
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Jose", "jose@@mail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 		
 	}
 
