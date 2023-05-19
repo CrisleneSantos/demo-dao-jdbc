871b32024d285fea243a273c0e4d8df366bbd51f
Crislene Santos <28mariavalentina@gmail.com>
2023-05-19 11:41:11 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id: " + newSeller.getId());
 		
+		System.out.println("\n=== Test 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Valentina Santos");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 		
 	}
 
